ae3afdd23889aaa50093df7058bf9d86e099fffb
Artifeex <dreamerandrew17@gmail.com>
2024-10-18 10:43:57 +0300

    initial commit

---
+++ src/main/java/com/sandr/web_library/WebLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sandr.web_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLibraryApplication.class, args);
+	}
+
+}
